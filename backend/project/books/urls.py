c931cad5bd8a2989e4f3a45137596c0b927e467f
Shahd Abdelnasser <147208563+shahodz@users.noreply.github.com>
2024-05-12 23:33:12 +0300

    added the user model

--- backend/project/books/urls.py
+++ backend/project/books/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import main_view, search_results, singup
+from .views import main_view, search_results, signup
 
 urlpatterns = [
     path('search/',main_view,name='main'),
     path('ser/',search_results,name='results'),
-    path('',singup,name='signup'),
+    path('',signup,name='signup'),
 
 ]

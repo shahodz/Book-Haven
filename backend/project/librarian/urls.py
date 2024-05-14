1886b77103662c11a8f89956c04597580a6bd654
Abdu-Ashry <111696075+Abdu-Ashry@users.noreply.github.com>
2024-05-14 16:53:16 +0300

    Added User HTML and CSS for(dashboards,profile

---
+++ backend/project/librarian/urls.py
@@ -0,0 +1,8 @@
+from django.urls import include, path
+from .views import admin_dashboard,adminprofile
+
+urlpatterns = [
+    path('admindashboard/',admin_dashboard,name='admindashboard'),
+    path('adminprofile/',adminprofile,name='adminprofile'),
+    
+]

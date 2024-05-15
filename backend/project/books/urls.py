a8a63d7a0270b4f4c4dd6bdeb153d76f80aac207
Duaa AbdelAti AbdelAzeem <164356690+DuaA-A@users.noreply.github.com>
2024-05-16 01:55:13 +0300

    .

--- backend/project/books/urls.py
+++ backend/project/books/urls.py
@@ -3,5 +3,5 @@ from .views import main_view, search_results
 
 urlpatterns = [
     path('',main_view,name='main'),
-    path('/results/',search_results,name='results'),
+    path('results/',search_results,name='results'),
 ]

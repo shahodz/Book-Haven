fae4c8fc434e40916bfed8f3069919e7980b6c17
Dalia Adel <daliaadel262@gmail.com>
2024-05-17 16:32:05 +0300

    renamed static files and fixed issues

--- backend/project/librarian/urls.py
+++ backend/project/librarian/urls.py
@@ -4,5 +4,6 @@ from .views import admin_dashboard,adminprofile
 urlpatterns = [
     path('admindashboard/',admin_dashboard,name='admindashboard'),
     path('adminprofile/',adminprofile,name='adminprofile'),
+    path('search/',include('books.urls')),
     
 ]

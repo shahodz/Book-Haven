f4379a5eb7db98e1ff7af8244c6703b1ad7c39a0
Mennaa6 <mennaabdelfattahhh@gmail.com>
2024-05-23 08:03:51 +0300

    deleted the extra model

--- backend/project/add_books/models.py
+++ backend/project/add_books/models.py
@@ -1,19 +1,6 @@
 from django.db import models
 
-class Book(models.Model):
-    name = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    image = models.URLField()
-    description = models.CharField(max_length=100000)
-    year = models.IntegerField()
-    publisher = models.CharField(max_length=100)
-    ISBN = models.CharField(max_length=100)
-    edition = models.CharField(max_length=100)
-    genre  = models.CharField(max_length=100)
-    language = models.CharField(max_length=100)
-    available=models.BooleanField(default=True)
-    def __str__(self):
-        return self.name
+
 
 
 # Create your models here.
--- backend/project/add_books/views.py
+++ backend/project/add_books/views.py
@@ -1,22 +1,7 @@
 from django.shortcuts import render,redirect
 from books.models import Book
 
-# def add(request):
-    
-#     bookname=request.POST.get('bookname')
-#     author=request.POST.get('author')
-#     description=request.POST.get('book_description')
-#     publisher=request.POST.get('Publisher')
-#     genre=request.POST.get('genre')
-#     language=request.POST.get('language')
-#     edition=request.POST.get('edition')
-#     publication=request.POST.get('date')
-#     isbn=request.POST.get('isbn')
-#     cover=request.POST.get('cover')
-#     data= Book(name=bookname,author=author,description=description,publisher=publisher,genre=genre,language=language,edition=edition,year=publication,ISBN=isbn,image=cover)
-#     if request.method=='POST':
-#         data.save()
-#     return render (request,'addApp/add_book.html')
+
 
 def add(request):
     if request.method == 'POST':
@@ -31,7 +16,7 @@ def add(request):
         isbn = request.POST.get('isbn')
         cover = request.POST.get('cover')
 
-        # Check if a book with the same ISBN already exists
+        
         if not Book.objects.filter(name=bookname).exists():
             Book.objects.create(
                 name=bookname,

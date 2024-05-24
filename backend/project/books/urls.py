edff4d4708fdb25fab9b1b4b55b469ee432aa4f7
Dalia Adel <daliaadel262@gmail.com>
2024-05-24 14:08:22 +0300

    final push

--- backend/project/books/urls.py
+++ backend/project/books/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 # project > books > urls.py
 urlpatterns = [
     path('view-books/', views.view_books, name='view_books'),
     path('borrowing/borrow2/', views.borrow_book2, name='borrow_book2'),
+    path('search/',include('search.urls')),
 ]
--- backend/project/search/views.py
+++ backend/project/search/views.py
@@ -1,39 +1,33 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from books.models import Book
-from django.db.models import Q
 import json
 
 def main_view(request):
     return render(request,'search.html')
 
 def search_results(request):
-    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-        res = None
-        search_key = request.POST.get('book')
+        result = []
+        search_input = request.POST.get('book')
         matching_books = []
-        matching_books += Book.objects.filter(
-            Q(name__icontains=search_key) |
-            Q(genre__icontains=search_key) |
-            Q(author__icontains=search_key)
-        )  
+        book_by_name = Book.objects.filter(name__icontains=search_input)
+        book_by_author = Book.objects.filter(author__icontains=search_input)
+        book_by_category = Book.objects.filter(genre__icontains=search_input)
+        matching_books = book_by_name | book_by_category | book_by_author
 
-        if len(search_key) > 0 and len(matching_books) > 0:
-            results = []
+        if len(search_input) > 0 and len(matching_books) > 0:
             for book in matching_books:
-                item = {
+                book_detail = {
                     'id':book.id,
                     'name': book.name,
                     'image': book.image,
                     'author': book.author,
                     'description': book.description,
                 }
-                results.append(item)
-            res = results
-        else:
-            res = "no book found"
-        return JsonResponse({'data': res})
-    return JsonResponse({})
+                result.append(book_detail)
+        elif len(matching_books)==0:
+            result = "no book found"
+        return JsonResponse({'data': result})
 
 
 # Create your views here.
